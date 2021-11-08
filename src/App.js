d86ee37b0e1c07c3e2ecaf6796e92b1dc3d59bdc
Diesel <jpedro.diesel@gmail.com>
2021-11-08 16:06:08 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
+
+// essa parte foi feita graças ao link
+// https://stackoverflow.com/questions/49850537/reactjs-pass-same-props-to-multiple-components
 
 class App extends React.Component {
   render() {
+    const cardProps = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: true,
+    };
     return (
-      <Form
-        cardName=""
-        cardDescription=""
-        cardAttr1=""
-        cardAttr2=""
-        cardAttr3=""
-        cardImage=""
-        cardRare=""
-        cardTrunfo=""
-        hasTrunfo=""
-        isSaveButtonDisabled=""
-        onInputChange=""
-        onSaveButtonClick=""
-      />
+      <>
+        <Form
+          { ...cardProps }
+          onInputChange=""
+          onSaveButtonClick=""
+        />
+        <Card
+          { ...cardProps }
+        />
+      </>
     );
   }
 }
